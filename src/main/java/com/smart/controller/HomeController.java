38477bd062d082bcddf5f2adbd222ba82073f8ed
Lavesh Satim <lavesh.git@gmail.com>
2022-12-18 23:18:33 +0530

    test dev yml files added

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -2,16 +2,19 @@ package com.smart.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController implements ErrorController {
 
 	@RequestMapping("/home")
-	public String getHomePage() {
+	public String getHomePage(Model model) {
+		model.addAttribute("name", "smart contact manager");
 		return "home";
 	}
 
+	// error handler
 	@RequestMapping("/error")
 	public String errorPage() {
 		return "errorpage";
