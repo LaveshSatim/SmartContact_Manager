d183e90f18ba28236f79bddb880f60a5b82bdb43
Lavesh Satim <lavesh.git@gmail.com>
2022-12-18 22:57:42 +0530

    error page and thymeleaf content added

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.smart.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class HomeController implements ErrorController {
 
 	@RequestMapping("/home")
 	public String getHomePage() {
 		return "home";
 	}
+
+	@RequestMapping("/error")
+	public String errorPage() {
+		return "errorpage";
+	}
+
 }
