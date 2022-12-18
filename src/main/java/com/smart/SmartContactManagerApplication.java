5f8e92f9c02b74021cfe9aae8b89d62a6b774f39
Lavesh Satim <lavesh.git@gmail.com>
2022-12-18 13:29:50 +0530

    profile check added

--- src/main/java/com/smart/SmartContactManagerApplication.java
+++ src/main/java/com/smart/SmartContactManagerApplication.java
@@ -1,13 +1,28 @@
 package com.smart;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.smart.config.ActiveProfiles;
+import com.smart.exception.SetProfileException;
+
 @SpringBootApplication
-public class SmartContactManagerApplication {
+public class SmartContactManagerApplication implements ApplicationRunner {
+
+	@Autowired
+	private ActiveProfiles activeProfiles;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartContactManagerApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws IllegalStateException {
+
+		activeProfiles.isProfileSet();
 	}
 
 }
